8c506db4148331c60ee97393852408ac268228ee
kancharajesh <rajeshkancha41@gmail.com>
2025-05-14 18:45:28 +0530

    api

--- Pages/Profile_page.js
+++ Pages/Profile_page.js
@@ -4,6 +4,7 @@ const { expect } = require("@playwright/test");
 exports.Profile_page = class Profile_page {
 
     constructor(page){
+        
         this.page = page;
 
         //Profile button.
