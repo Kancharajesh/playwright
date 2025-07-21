a446dce29a92cd6a98a737e2a3db362f5cb5525b
kancharajesh <rajeshkancha41@gmail.com>
2025-07-21 19:18:35 +0530

    mobile superj

--- tests/dummy.spec.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('open Playwright website', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwright/);
-});
